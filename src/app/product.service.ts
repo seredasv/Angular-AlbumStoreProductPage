7c054181725d26fe078d18613438020ec566ae26
Sergey Sereda <sergey.sereda@avanade.com>
2019-03-25 22:58:08 +0100

    Displaying a List of Products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,12 @@ import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
 import {Album} from './album';
+import {Product} from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {
   }
@@ -17,4 +19,9 @@ export class ProductService {
       .map((response) => <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http
+      .get(this._productsUrl)
+      .map((response) => <Product[]>response.json());
+  }
 }
